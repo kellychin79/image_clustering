294e41930b68fde361df4b300d0cfdc5fd4e27f6
Kelly Lee <kellychin79@gmail.com>
2023-01-26 01:39:52 -0500

    complete the writeup

--- README.md
+++ README.md
@@ -8,32 +8,35 @@ It starts with calculating the distance between all data points and arbitrary st
 
 I experimented on numerous projects using K-means, such as the classic iris dataset, gambler dataset, and homeowner dataset. So far, image clustering is the most interesting one - it is a primitive form of image filter!
 
-A photo has millions of pixels, each of which represents a unique RGB data value (Red, Green, Blue). Below, you can find the ideal image sizes in terms of resolution.
+In this project, I wanted to transform a photo of me, holding a US flag in a red dress on the Fourth of July, into a photo of 2 colors (black and white), 3 colors, 5 colors, and 10 colors. Let's get started!
 
-- 4 x 6” 1200 x 1800 pixels = 2.2 million pixels
-- 5 x 7” 1500 x 2100 pixels = 3.2 million pixels
-- 8 x 10 “ 2400 x 3000 pixels = 7.2 million pixels
-- Instagram posts 1080 x 1080 pixels = 1.2 million pixels 
-- Pinterest pins 1000 x 1500 pixels = 1.5 million pixels
-Source: https://www.adobe.com/uk/creativecloud/photography/discover/standard-photo-sizes.html#:~:text=An%20image%20size%20of%201280,is%20also%20common%20in%20filmmaking
 
-- TikTok videos 1080×1920 pixels = 2.1 million pixels
-Source: https://www.winxdvd.com/streaming-video/tiktok-video-resolution-dimensions.htm
+A photo has millions of pixels, each of which represents a unique **RGB data value** (Red, Green, Blue). Below, you can find different ideal image sizes in terms of resolution.
 
-In this project, I wanted to transform a photo of me, holding a US flag in a red dress on the Fourth of July, into a photo of 4 colors, 2 colors (black and white), and 10 colors. 
+- Source: https://www.adobe.com/uk/creativecloud/photography/discover/standard-photo-sizes.html#:~:text=An%20image%20size%20of%201280,is%20also%20common%20in%20filmmaking
+	- 4 x 6” 1200 x 1800 pixels = 2.2 million pixels
+	- 5 x 7” 1500 x 2100 pixels = 3.2 million pixels
+	- 8 x 10 “ 2400 x 3000 pixels = 7.2 million pixels
+	- Instagram posts 1080 x 1080 pixels = 1.2 million pixels 
+	- Pinterest pins 1000 x 1500 pixels = 1.5 million pixels
+
+- Source: https://www.winxdvd.com/streaming-video/tiktok-video-resolution-dimensions.htm
+	- TikTok videos 1080×1920 pixels = 2.1 million pixels
+
+
 
 ## Descriptions
 
-First of all, I reshaped the original 3-dimensional image file (2,048 x 1,548 x 3) into a very long 2-dimensional array (3,170,304 x 3). 
+First of all, I reshaped the original 3-dimensional image file (2,048 x 1,548 x 3) into a very long 2-dimensional array (3,170,304 x 3). Note the 3 stores the RGB values respectively.
 
-Next, I applied the K-means clustering algorithm, which went through the array of RGB data values and then classified similar ones into the same group. 
+Next, I applied the K-means clustering algorithm. It went through the array of RGB data values, calculated for iterations until convergence, and lastly classified similar ones into the same group. 
 
 
 ## Summary 
-The processing time is not linear but exponential because of the big dataset. Here are the observations: k=2 clusters took 14 iterations until convergence (black and white); k=3 clusters took 33 iterations until convergence; k=5 took 70 iterations; k=10 took 166 iterations (almost carrying the original style). 
+The processing time is not linear but exponential because of the big dataset. Here are the observations: k=2 clusters took 14-16 iterations until convergence (black and white); k=3 clusters took 30-33 iterations until convergence; k=5 took 70-85 iterations; k=10 took 90-166 iterations (almost carrying the original style). 
 
-Accordingly, thousands of colors in the photo - dark, bright, dim, and vibrant - were compressed and the image style amazingly changed - just like the filters in Snapseed, Instagram, and other photo-editing apps!
+Thousands of colors in the photo were grouped and compressed - dark, bright, dim, and vibrant. Accordingly, the image style amazingly changed - just like the filters in Snapseed, Instagram, and other photo-editing apps!
 
-
+![alt text](https://github.com/kellychin79/image_clustering/blob/main/output.png?raw=true)
 
 
--- image_stylized_recoloring.py
+++ image_stylized_recoloring.py
@@ -5,11 +5,12 @@
 
 # ![image.png](attachment:image.png)
 
-# In[50]:
+# In[68]:
 
 
 from PIL import Image
 from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 import numpy as np
 import random
@@ -17,7 +18,7 @@ import random
 
 # ### Read image and store it as an 3 dimensional array
 
-# In[4]:
+# In[69]:
 
 
 img = Image.open(f"photo.jpeg")
@@ -26,7 +27,7 @@ print(img)
 
 # In digital imaging, a pixel(or picture element) is the smallest item of information in an image. 
 
-# In[45]:
+# In[70]:
 
 
 img_arr = np.array(img, dtype='int32')
@@ -41,20 +42,18 @@ print('img_arr\'s last pixel:','\n',img_arr[-1][-1])
 
 # ### Use the 3 dimensional array to display the image
 
-# In[47]:
+# In[71]:
 
 
 arr = img_arr.astype(dtype='uint8')
+print(type(arr))
+print('\nShape:', arr.shape, '\n')
 print(arr[0])
 
 
-# `Image.fromarray`
-# 
-# Creates an image memory from an object exporting the array interface (using the buffer protocol).
-# 
 # `matplotlib.pyplot.imshow(X)`
 # 
-# Display data as an image
+# Display data as an *inline* image
 # 
 # `X`: array-like or PIL image. The image data. Supported array shapes are:
 # 
@@ -63,15 +62,28 @@ print(arr[0])
 # - (M, N, 4): an image with RGBA values (0-1 float or 0-255 int), i.e. including transparency.
 # - The first two dimensions (M, N) define the rows and columns of the image. Out-of-range RGB(A) values are clipped.
 
-# In[14]:
+# In[104]:
+
+
+imshow(arr)
+plt.title('Original')
+
+
+# `Image.fromarray`
+# 
+# Creates an image memory from an object exporting the array interface (using the buffer protocol).
+# 
+# 
+
+# In[73]:
 
 
 img = Image.fromarray(arr, mode='RGB') # (3x8-bit pixels, true color)
+print(type(img))
+# img
 
-imshow(np.asarray(img))
 
-
-# In[17]:
+# In[74]:
 
 
 try:
@@ -80,16 +92,17 @@ except ValueError:
     print('Failed - CMYK is for 4 dimensional array')
 
 
-# ### reshape the matrix into "img_reshaped" by transforming "img_arr" from a "3-D" matrix to a flattened "2-D" matrix which has 3 columns corresponding to the RGB values for each pixel
+# ### Reshape the matrix by transforming "img_arr" from a "3-D" matrix to a flattened "2-D" matrix  
+# It has 3 columns corresponding to the RGB values for each pixel
 
-# In[21]:
+# In[75]:
 
 
 img_reshaped = img_arr.reshape([img_arr.shape[0]*img_arr.shape[1], 3])
 print(img_reshaped.shape) # 2048*1548 = 3170304
 
 
-# In[49]:
+# In[76]:
 
 
 print('img_reshaped\'s 1st pixel:','\n',img_reshaped[0])
@@ -99,9 +112,9 @@ print('img_reshaped\'s last pixel:','\n',img_reshaped[-1])
 
 # The two examples above are same as the orginal 3-dimensional array
 
-# ### create k-means function that groups a large volume of inputs into a small number of clusters
+# ### Create k-means function that groups a large volume of inputs into a small number of clusters
 
-# In[60]:
+# In[77]:
 
 
 print('# Low and high are both specified.')
@@ -110,15 +123,15 @@ print('\n# If high is None (the default), then results are from [0, low).')
 print(np.random.randint(2, size=10)) 
 
 
-# In[73]:
+# In[78]:
 
 
 # initialize k centers randomly
 def initialize_centers(X, k):
-    return X[np.random.randint(len(X), size=k): ] # slicing to returns centers as a (k x d) numpy array.
+    return X[np.random.randint(len(X), size=k), : ] # slicing to returns centers as a (k x d) numpy array.
 
 
-# In[74]:
+# In[79]:
 
 
 # computes a distance matrix, storing the squared distance from point x to center j. 
@@ -134,7 +147,7 @@ def compute_squared_distance(X, centers):
     return S
 
 
-# In[75]:
+# In[80]:
 
 
 def compute_squared_distance_v2(X, centers):
@@ -148,7 +161,7 @@ def compute_squared_distance_v2(X, centers):
     return b
 
 
-# In[83]:
+# In[81]:
 
 
 # Given a clustering (i.e., a set of points and assignment of labels), compute the center of each cluster.
@@ -171,7 +184,7 @@ def find_centers(X, labels):
     
 
 
-# In[77]:
+# In[82]:
 
 
 # use the squared distance matrix to find each point's minimum squared distance and assign a "cluster label" to it
@@ -179,7 +192,7 @@ def assign_cluster_labels(S):
     return np.argmin(S, axis = 1)
 
 
-# In[78]:
+# In[83]:
 
 
 # Given the squared distances, return the within-cluster sum of squares.
@@ -187,7 +200,7 @@ def calculate_wcss(S):
     return np.sum(np.amin(S, axis=1)) # Return the minimum of an array or minimum along an axis.
 
 
-# In[80]:
+# In[84]:
 
 
 def kmeans(X, k,
@@ -226,20 +239,184 @@ def kmeans(X, k,
     return labels
 
 
-# ### Apply the k-means function to divide the image in 3 clusters. The result would be a vector named labels, which assigns the label to each pixel.
+# ### Apply the k-means function to divide the image in 3 clusters. 
+# The result would be a vector named labels, which assigns the label to each pixel.
 # 
 
-# In[84]:
+# In[85]:
 
 
 clusters = kmeans(img_reshaped, k=3, starting_centers=img_reshaped[[0, 0], :],max_steps=np.inf)
 
 
-# In[85]:
+# In[86]:
 
 
 clusters
 
+
+# In[87]:
+
+
+set(clusters)
+
+
+# Specifying 1 point by [0,0] was a wrong setup, it then returned only 1 cluster (a.k.a label 0).
+
+# In[88]:
+
+
+clusters2 = kmeans(img_reshaped, k=3, starting_centers=None,max_steps=np.inf)
+
+
+# In[89]:
+
+
+clusters2
+
+
+# In[90]:
+
+
+set(clusters2)
+
+
+# It looks more reasonable now - three clusters (three labels).
+
+# ### Calculate the mean of each cluster 
+
+# In[91]:
+
+
+centers = {}
+centers[0] = np.mean(img_reshaped[clusters2==0], axis = 0)
+centers[1] = np.mean(img_reshaped[clusters2==1], axis = 0)
+centers[2] = np.mean(img_reshaped[clusters2==2], axis = 0)
+centers
+
+
+# ### Generate a matrix of the same dimensions, where each pixel is replaced by the cluster center to which it belongs.
+
+# In[92]:
+
+
+img_clustered = np.array([centers[i] for i in clusters2])
+img_clustered
+
+
+# ### Display the clustered image
+
+# In[93]:
+
+
+arr_clustered = img_clustered.astype(dtype='uint8')
+print(type(arr_clustered))
+print()
+print(arr_clustered)
+
+
+# In[94]:
+
+
+imshow(arr_clustered)
+
+
+# In[95]:
+
+
+arr_clustered.shape
+
+
+# **Problem** The image is not properly showing because of the wrong shape - 3,170,304 meaning one long line of pixels. Note that the y axis is up to 10e6. 
+# **Solution** It should be 3-dimensional of (2048, 1548, 3)  - 2048 X 1548 representing the 2D picture.
+
+# In[96]:
+
+
+R, G, B = arr.shape
+print(R, G, B)
+
+
+# In[97]:
+
+
+arr_clustered2 = arr_clustered.reshape([R, G, B])
+print(type(arr_clustered2))
+print("\nShape:", arr_clustered2.shape, '\n')
+print(arr_clustered2[0])
+
+
+# In[98]:
+
+
+imshow(arr_clustered2)
+plt.title('K=3 Clusters')
+
+
+# ### Experiment with different clusters! 
+
+# In[99]:
+
+
+def one_stop_function(original_image, reshaped_image, k, starting_centers, max_steps):
+    # Apply the k-means function
+    cluster = kmeans(reshaped_image, k=k, starting_centers=None,max_steps=np.inf)
+    assert len(set(cluster)) == k, "Number of identified clusters does not match the specified cluster numbers."
+    
+    # Calculate the mean of each cluster
+    centers = {}
+    for i in range(len(set(cluster))):
+        centers[i] = np.mean(reshaped_image[cluster==i], axis = 0)
+    
+    # Generate a matrix 
+    img_clustered = np.array([centers[i] for i in cluster])
+    
+    # Reshape the matrix to the shape of the original image
+    R, G, B = original_image.shape
+    return img_clustered.astype(dtype='uint8').reshape([R, G, B])
+
+
+# In[100]:
+
+
+k_2 = one_stop_function(original_image=img_arr,
+                        reshaped_image=img_reshaped, k=2, starting_centers=None, max_steps=np.inf)
+
+# Display the clustered image 
+imshow(k_2)
+plt.title('K=2 Clusters')
+
+
+# In[101]:
+
+
+k_5 = one_stop_function(original_image=img_arr,
+                        reshaped_image=img_reshaped, k=5, starting_centers=None, max_steps=np.inf)
+
+# Display the clustered image 
+imshow(k_5)
+plt.title('K=5 Clusters')
+
+
+# In[103]:
+
+
+k_10 = one_stop_function(original_image=img_arr,
+                         reshaped_image=img_reshaped, k=10, starting_centers=None, max_steps=np.inf)
+
+# Display the clustered image 
+imshow(k_10)
+plt.title('K=10 Clusters')
+
+
+# ### Summary: 
+# The processing time is not linear but exponential because of the big dataset.
+# 
+# - k=2 clusters took 14-16 iterations until convergence
+# - k=3 clusters took 30-33 iterations until convergence
+# - k=5 clusters took 70-85 iterations until convergence
+# - k=10 clusters took 90-166 iterations until convergence
+# 
 
 # In[ ]:
 
